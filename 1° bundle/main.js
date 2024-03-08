514d3bef6ff0f9ad6c5a1a55663d0cbed6b008f8
JacopoLombardi <lombajacopo@gmail.com>
2024-03-08 18:48:26 +0100

    [add] Es 1 Finished

--- 1° bundle/main.js
+++ 1° bundle/main.js
@@ -11,9 +11,18 @@
 
 
 // ESERCIZIO 1
-for (let i = 0; i > 5; i++) {
+for (let i = 0; i < 5; i++) {
     console.log(i);
 }
+
+
+    // 1- Che cosa fa questo codice?            -Stampa in console l'indice per 5 volte.
+    // 2- Sono presenti errori di sintassi?     -E' presente 1 errore a riga 14 '>'.
+    // 3- Sono presenti errori logici?          -NO.
+
+
+
+
 
 
 // ESERCIZIO 2
@@ -25,23 +34,33 @@ function addIfEven(num) {
 }
 
 
-// ESERCIZIO 3
-function loopToFive() {
-    for (let i = 0, i < 5, i++) {
-        console.log(i);
-    }
-}
 
 
-// ESERCIZIO 4 (suggerimento: ci sono 7 errori)
-function displayEvenNumbers() {
-    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-    let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
-            evenNumbers.push(i);
-        }
-        return evenNumbers;
-    }
-}
-displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+
+
+
+// // ESERCIZIO 3
+// function loopToFive() {
+//     for (let i = 0, i < 5, i++) {
+//         console.log(i);
+//     }
+// }
+
+
+
+
+
+
+
+// // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
+// function displayEvenNumbers() {
+//     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+//     let evenNumbers = [];
+//     for (let i = 0; i < numbers.length - 1; i++;) {
+//         if (numbers % 2 = 0); {
+//             evenNumbers.push(i);
+//         }
+//         return evenNumbers;
+//     }
+// }
+// displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
