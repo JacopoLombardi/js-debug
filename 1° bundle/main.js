bd454a0fb3b37f882a395c62a9d6690d417b6861
JacopoLombardi <lombajacopo@gmail.com>
2024-03-08 19:15:52 +0100

    [add] Es 4 Finished

--- 1° bundle/main.js
+++ 1° bundle/main.js
@@ -62,15 +62,20 @@ function loopToFive() {
 
 
 
-// // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
-// function displayEvenNumbers() {
-//     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-//     let evenNumbers = [];
-//     for (let i = 0; i < numbers.length - 1; i++;) {
-//         if (numbers % 2 = 0); {
-//             evenNumbers.push(i);
-//         }
-//         return evenNumbers;
-//     }
-// }
-// displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+// ESERCIZIO 4 (suggerimento: ci sono 7 errori)
+function displayEvenNumbers() {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    const evenNumbers = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 === 0) {
+            evenNumbers.push(numbers[i]);
+        };
+    };
+    return evenNumbers;
+};
+displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+
+
+    // 1- Che cosa fa questo codice?           -Quando viene invocata la funzione parte il ciclo controllando dentro l'array 'numbers' quali numeri sono pari, se sono pari li pusha dentro l'array 'evenNumbers'.
+    // 2- Sono presenti errori di sintassi?    -SI sono presenti 6 errori, a riga 67 e 68 'let'  ---  a riga 69 '-1' e ';'  ---  a riga 70 'numbers' , '=' , ';'  ---  a riga 71 'i'.
+    // 3- Sono presenti errori logici?         -SI   a riga 73 'return evenNumbers;' non deve restituire l'array 'evenNumbers' dentro il ciclo, ma una volta terminato il ciclo.
